cd0d95b779fd2e997e3fbab42adaed87b519bf57
shantanusamant <samant.shantanu17@gmail.com>
2023-02-28 21:17:46 -0500

    Started working on brain storming for app design. Have picked a dataset on number of electric vehicle charging stations city wise, and analysie and check hypothesis, if political sentiment on Electric vehicle subisidy has an effect on the average number of electric charging stations per capita, by checking difference between Democratic Counties (with members of congress from the democratic party) and Republican counties (With members of congress from the Republican Party)
    I am currently in the process of finding out datasets online and check if there are some that readily have county data along with their GEOIDS so that they can be merged with the data in Alt_Fuel_Stations.csv file

---
+++ project/app.R
@@ -0,0 +1,37 @@
+#Author- Shantanu Samant
+
+
+#THE AIM OF THIS FINAL PROJECT IS TO WORK ON ANSWERING AND INVESTIGATING THE FOLLOWING QUESTION VISUALLY
+#Question How is the EV charging station infrastructure at any county in USA, 
+#co-related/associated with the political inclination of that location and its population.
+#Inputs: State, EVSE number slider,
+
+# Libraries
+library(shiny)
+library(shinythemes)
+library(leaflet)
+library(leaflet.extras)
+library(sf)
+library(shinyjs)
+library(dplyr)
+library(shinydashboard)
+library(reshape2)
+library(dplyr)
+library(plotly)
+library(ggplot2)
+library(shiny)
+library(hms)
+library(DT)
+library(stringr)
+library(tools)
+library(readr)
+library(tidyverse)
+
+
+
+
+ui <- fluidPage()
+
+server <- function(input, output) {}
+
+shinyApp(ui = ui, server = server)
